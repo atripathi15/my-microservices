37538ece3460c87082bbd899828a1b15a9a1ff2b
atripathi15 <er.ashishtripathi@gmail.com>
2022-05-22 00:56:34 +0530

    first commit

---
+++ sociamedia-login-demo/sociamedia-login-demo/src/main/java/com/ashish/sociamedia/SpringSecuritySociamediaLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.sociamedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySociamediaLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySociamediaLoginDemoApplication.class, args);
+	}
+
+}
---
+++ sociamedia-login-demo/sociamedia-login-demo/src/test/java/com/ashish/sociamedia/SpringSecuritySociamediaLoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.sociamedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySociamediaLoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
