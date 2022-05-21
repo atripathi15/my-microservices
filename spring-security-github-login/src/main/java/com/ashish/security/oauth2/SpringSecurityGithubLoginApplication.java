26cb46e2fcd39f235772eda8897a4849c63f8a10
atripathi15 <er.ashishtripathi@gmail.com>
2022-05-22 00:57:17 +0530

    first commit

---
+++ spring-security-github-login/src/main/java/com/ashish/security/oauth2/SpringSecurityGithubLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.security.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGithubLoginApplication.class, args);
+	}
+
+}
---
+++ spring-security-github-login/src/test/java/com/ashish/security/oauth2/SpringSecurityGithubLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.security.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGithubLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
