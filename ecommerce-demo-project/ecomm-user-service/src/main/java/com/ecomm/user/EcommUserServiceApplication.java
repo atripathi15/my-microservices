cdc2c02191d0a43829b042ac5b08013de5a60478
atripathi15 <er.ashishtripathi@gmail.com>
2022-08-14 15:40:16 +0530

    discovery server configured

--- ecommerce-demo-project/ecomm-user-service/src/main/java/com/ecomm/user/EcommUserServiceApplication.java
+++ ecommerce-demo-project/ecomm-user-service/src/main/java/com/ecomm/user/EcommUserServiceApplication.java
@@ -3,9 +3,11 @@ package com.ecomm.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class EcommUserServiceApplication {
 
 	public static void main(String[] args) {
