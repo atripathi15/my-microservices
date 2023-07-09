5a847cc338682288c6227fb23abcf811b229fe13
atripathi15 <er.ashishtripathi@gmail.com>
2023-07-09 12:58:08 +0530

    code updated

--- spring-security-jwt/src/main/java/com/ashish/security/HomeController.java
+++ spring-security-jwt/src/main/java/com/ashish/security/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.ashish.security.service.MyUserDetailsService;
 import com.ashish.security.util.JwtUtil;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
 	
 	@Autowired
