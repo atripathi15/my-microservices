d1d9bdb36aeaed93b4d37b58a74b4987ca77b8c4
atripathi15 <er.ashishtripathi@gmail.com>
2025-10-04 12:05:50 +0530

    cors

--- ecommerce-demo-project/product-service/src/main/java/com/ashish/product/controller/ProductController.java
+++ ecommerce-demo-project/product-service/src/main/java/com/ashish/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ashish.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.ashish.product.repository.SellerRepository;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
